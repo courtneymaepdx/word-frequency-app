92b253f3db0bae3c199ca8712ff8d7d81fb66ac8
Courtney Phillips <courtney.mae.phillips@gmail.com>
2015-04-24 11:50:10 -0700

    Include integration testing.

---
+++ spec/word_frequency_integration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('the word frequency path', {:type => :feature}) do
+  it('processes the users entries and returns number occurrences of one within the other') do
+    visit('/')
+    fill_in('phrase', :with => 'You are pretty cool, yeah I guess you are cool.')
+    fill_in('word', :with => 'cool')
+    click_button('Send')
+    expect(page).to have_content(2)
+  end
+end
