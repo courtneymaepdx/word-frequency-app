a1be40438873f57267d87a0c1215274bfaf15ef6
Courtney Phillips <courtney.mae.phillips@gmail.com>
2015-04-24 10:10:15 -0700

    Recognize NO presence of given word in phrase. Additional test. Test pass.

--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -4,6 +4,11 @@ require('word_frequency')
 describe('String#word_frequency') do
   it("recognizes if the word is present in the phrase") do
     expect("I want to shower in hamburgers".word_frequency("hamburgers")).to(eq(true))
-end
+  end
+
+#need this second rspec, if the boolean from above will return false as well anyway?
+  it("recognizes if the word is NOT present in the phrase") do
+    expect("My cat struggles with the intricacies of foreign policy".word_frequency("embargo")).to(eq(false))
+  end
 
 end
