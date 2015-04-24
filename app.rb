c1788335e507e509ab5c212b31a7fbd419000e25
Courtney Phillips <courtney.mae.phillips@gmail.com>
2015-04-24 12:41:39 -0700

    Does not provide grammatically incorrect plural responses when result is 1 or 0.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word_frequency')
+also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
@@ -10,5 +11,12 @@ get('/word_frequency') do
   @phrase = params.fetch('phrase')
   @word = params.fetch('word')
   @occurrence_number = @phrase.word_frequency(@word)
+  if @occurrence_number == 0
+    @occurrence_number = "Zero times! That word isn't even in this phrase!"
+  elsif @occurrence_number == 1
+    @occurrence_number = "Once!"
+  else
+    puts "times!"
+  end
   erb(:word_frequency)
 end
