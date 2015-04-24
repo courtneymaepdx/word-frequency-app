0d7765a67462b7816f25ce5357f4664bf686f5fe
Courtney Phillips <courtney.mae.phillips@gmail.com>
2015-04-24 10:54:30 -0700

    Test for ability to count occurrences of word despite any punctuation attached.

--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -16,7 +16,10 @@ describe('String#word_frequency') do
   end
 
   it("counts words with irregular casing") do
-    expect("The ever-impatient gaggle of toddlers began chanting cake CAKE CAKE caKe cAKE".word_frequency("cake")).to(eq(5))
+    expect("HI honEy its DAD jusst TRYING OUT my nEw phone LOL this txt thing SURE is neeat, LoL".word_frequency("lol")).to(eq(2))
   end
 
+  it("counts words regardless of any extra punctuation attached") do
+    expect("The roving, ever-impatient mob of toddlers began chanting 'cake, CAKE, CAKE, caKe, cAKE!'".word_frequency("cake")).to(eq(5))
+  end
 end
